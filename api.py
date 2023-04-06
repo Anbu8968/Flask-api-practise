7fc5723bf1b5082286061bfc442fcec053b4c0f4
Anbu V <60262202+Anbu8968@users.noreply.github.com>
2023-04-06 10:01:57 +0530

    Update api.py

--- api.py
+++ api.py
@@ -60,7 +60,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config["MAIL_USERNAME"]="anbu2872001@gmail.com"
-app.config["MAIL_PASSWORD"]="wckqpygpmzrzsrjm"
+app.config["MAIL_PASSWORD"]="enter password"
 # app.config["MAIL_USE_TLS"]="True"
 app.config["MAIL_USE_SSL"]=False
 
